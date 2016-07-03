556fd5be727bf28475d57c5c9efca309727f7950
Heng Yuan <superduperhengyuan@gmail.com>
2016-07-02 21:10:36 -0700

    Update with license for the code used in DoubleUtils.

--- cookjson-core/src/main/java/org/yuanheng/cookjson/DoubleUtils.java
+++ cookjson-core/src/main/java/org/yuanheng/cookjson/DoubleUtils.java
@@ -18,10 +18,32 @@ package org.yuanheng.cookjson;
 import java.math.BigInteger;
 
 /**
- * The algorithm and some code used here are from
+ * The algorithm and code used here are from
  * 	Jaffer, Aubrey. "Easy Accurate Reading and Writing of Floating-Point
  * 	Numbers." arXiv preprint arXiv:1310.8121 (2013).
  *  (<a href="https://arxiv.org/pdf/1310.8121.pdf">PDF</a>)
+ * <p>
+ * And here is the license given from him:
+ * <pre>
+ * Copyright (c) Aubrey Jaffer 2014
+ * Permission to copy this software, to modify it, to redistribute it,
+ * to distribute modified versions, and to use it for any purpose is
+ * granted, subject to the following restrictions and understandings.
+ * 
+ * 1.  Any copy made of this software must include this copyright notice
+ * in full.
+ *
+ * 2.  I have made no warranty or representation that the operation of
+ * this software will be error-free, and I am under no obligation to
+ * provide any services, by way of maintenance, update, or otherwise.
+ *
+ * 3.  In conjunction with products arising from the use of this
+ * material, there shall be no use of my name in any advertising,
+ * promotional, or sales literature without prior written consent in
+ * each case.
+ * </pre>
+ * <p>
+ * I made minor modifications to fit my need.
  *
  * @author	Heng Yuan
  */
